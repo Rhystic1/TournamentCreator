f0531d67cfdb9350d4d47fc1c2dc9ddc30d813f9
Alien <peterdavidson.23.94@gmail.com>
2022-11-18 15:47:59 +0000

    Added some stuff
    - Added audit for keeping history of changes made in the tournament
    - Added the ability to highlight players for future stuff
    - Removed old src/Groups file
    - Added a debug string to catch any weirdness

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,12 +1,17 @@
 package logic;
+
+import javax.swing.SwingUtilities;
+
 import ui.FrontEnd;
 
 /* Stuff to do: implement next stages, move most methods to their own classes, etc. */
 
 public class Main {
 	public static void main(String[] args) {
-		Tournament tournament = new Tournament().generateTournament();
-		FrontEnd frontEnd = new FrontEnd();
-		frontEnd.addTournament(tournament);
+		SwingUtilities.invokeLater(() -> {
+			Tournament tournament = new Tournament().generateTournament();
+			FrontEnd frontEnd = new FrontEnd();
+			frontEnd.addTournament(tournament);
+		});
 	}
 }
