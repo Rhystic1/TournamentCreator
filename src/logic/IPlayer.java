888fbf4feebb7eff7b059da7ed78f154e357cb07
rhystic1 <rhysticone@gmail.com>
2022-12-06 09:56:27 +0000

    - Backend work to add a new Player class and relevant interface (NOTE: The "Players" class will soon be renamed to avoid confusion. - Added a new "phase" enum to determine the current status of a player.

---
+++ src/logic/IPlayer.java
@@ -0,0 +1,15 @@
+package logic;
+
+public interface IPlayer {
+    String getName();
+    void setName(String name);
+
+    int getPlayerScore();
+    void setPlayerScore(int score);
+
+    int getStartingGroup();
+    public void setStartingGroup(int startingGroup);
+
+    boolean isEliminated();
+    void setEliminated(boolean isEliminated);
+}
